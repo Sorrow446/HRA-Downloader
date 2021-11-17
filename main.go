6fb19fbb4108a48a1c72758e37de503e9f918da7
Sorrow446 <47045733+Sorrow446@users.noreply.github.com>
2021-11-17 06:58:55 +0000

    Add support for stream-app.highresaudio.com urls, small cleanup.

--- main.go
+++ main.go
@@ -254,11 +254,8 @@ func getMeta(albumId, userData, lang string) (*AlbumMeta, error) {
 }
 
 func makeDir(path string) error {
-	err := os.Mkdir(path, 0755)
-	if err != nil && !errors.Is(err, os.ErrExist) {
-		return err
-	}
-	return nil
+	err := os.MkdirAll(path, 0755)
+	return err
 }
 
 func fileExists(path string) (bool, error) {
@@ -272,13 +269,18 @@ func fileExists(path string) (bool, error) {
 }
 
 func checkUrl(url string) bool {
-	regexString := `^https://www.highresaudio.com/(?:en|de)/album/view/[a-z\d]+/[a-z\d-]+$`
-	regex := regexp.MustCompile(regexString)
-	match := regex.FindStringSubmatch(url)
-	if match == nil {
-		return false
+	regexes := [2]string{
+		`^https://www.highresaudio.com/(?:en|de)/album/view/[a-z\d]+/[a-z\d-]+$`,
+		`^https://stream-app.highresaudio.com/album/[a-z\d]{8}-[a-z\d]{4}-[a-z\d]{4}-[a-z\d]{4}-[a-z\d]{12}$`,
 	}
-	return true
+	for _, regexString := range regexes {
+		regex := regexp.MustCompile(regexString)
+		match := regex.MatchString(regexString)
+		if match {
+			return true
+		}
+	}
+	return false
 }
 
 func parseAlbumMeta(meta *AlbumMeta) map[string]string {
@@ -416,11 +418,7 @@ func writeTags(trackPath, coverPath string, tags map[string]string) error {
 	return err
 }
 
-func downloadBooklet(path, url string, download bool) error {
-	if url == "" || !download {
-		return nil
-	}
-	fmt.Println("Downloading booklet...")
+func downloadBooklet(path, url string) error {
 	f, err := os.OpenFile(path, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0755)
 	if err != nil {
 		return err
@@ -439,10 +437,10 @@ func downloadBooklet(path, url string, download bool) error {
 }
 
 func checkAvail(availAt time.Time) bool {
-	return time.Now().Unix() > availAt.Unix()
+	return time.Now().Unix() >= availAt.Unix()
 }
 
-func init() {
+func main() {
 	fmt.Println(`                                                           
  _____ _____ _____    ____                _           _         
 |  |  | __  |  _  |  |    \ ___ _ _ _ ___| |___ ___ _| |___ ___ 
@@ -457,9 +455,6 @@ func init() {
 	if err != nil {
 		panic(err)
 	}
-}
-
-func main() {
 	cfg, err := parseCfg()
 	if err != nil {
 		errString := fmt.Sprintf("Failed to parse config file.\n%s", err)
@@ -519,9 +514,12 @@ func main() {
 			coverPath = ""
 		}
 		bookletPath := filepath.Join(albumPath, "booklet.pdf")
-		err = downloadBooklet(bookletPath, meta.Data.Results.Booklet, cfg.DownloadBooklets)
-		if err != nil {
-			fmt.Println("Failed to download booklet.\n", err)
+		if meta.Data.Results.Booklet != "" && cfg.DownloadBooklets {
+			fmt.Println("Downloading booklet...")
+			err = downloadBooklet(bookletPath, meta.Data.Results.Booklet)
+			if err != nil {
+				fmt.Println("Failed to download booklet.\n", err)
+			}
 		}
 		trackTotal := len(meta.Data.Results.Tracks)
 		for trackNum, track := range meta.Data.Results.Tracks {
